a384f91222bde346513bd7d93cc2cd2f63c451d5
insharahkalam <insharahkalam19@gmail.com>
2025-07-17 03:08:11 +0500

    change

--- app.js
+++ app.js
@@ -236,7 +236,7 @@ async function displayUserProfile() {
           "https://www.gravatar.com/avatar/?d=mp";
         document.getElementById("profile-name").textContent =
           user.user_metadata?.full_name ||
-           user.user_metadata?.name || user.email;
+           user.user_metadata?.name;
         document.getElementById("profile-email").textContent = user.email;
       }
 
